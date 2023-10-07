5be7257030a5ab45c77bd68472e562f42bcea7a8
Iurii Ovsiannikov <jgoldste@z2t7c2.42bangkok.com>
2023-10-07 14:20:31 +0700

    ex00_is_done_Oct7_14-20

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:17:35 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/10/06 19:32:21 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/10/07 14:19:09 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,36 @@
 int	main() {
 
 	int number = 0;
+	int	action = 0;
 
-	while (number != -1)
+	while (true)
 	{	
 		try {
 			
-			std::cout << "Input integer number for creating : ";
+			std::cout << "Input grade level to create bureaucrat (Exit: [-1]): ";
 			std::cin >> number;
-			Bureaucrat* a = new Bureaucrat("OnHeap", number);
-			a->incrementGrade();
-			std::cout << *a << std::endl;
-			delete a;
+			if (number == -1)
+				return 0;
+			Bureaucrat* a = new Bureaucrat("OnHeap", number);\
+			std::cout << "Created: " << *a << std::endl;
+			std::cout << "To leave bureaucrat alone, input [0]" << std::endl;
+			std::cout << "For increment input [1]" << std::endl;
+			std::cout << "For decrement input [-1]" << std::endl;
+			std::cin >> action;
+			try {
+				if (action < -1 || action > 1)
+					throw std::invalid_argument("Invalid action mode");
+				if (action == 1)
+					a->incrementGrade();
+				else if (action == -1)
+					a->decrementGrade();
+				std::cout << "Updated: " << *a << std::endl;
+				delete a;
+			}
+			catch (const std::exception& e) {
+				std::cerr << "Caught an exception: " << e.what() << std::endl;
+				delete a;
+			}
 		}
 		catch (const std::exception& e) {
 			std::cerr << e.what() << std::endl;
