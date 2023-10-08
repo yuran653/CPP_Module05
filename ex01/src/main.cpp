6d94edcb3dcecf9d31d28b8090e7ec0e22002a82
Iurii Ovsiannikov <jgoldste@z2t7c3.42bangkok.com>
2023-10-08 14:35:24 +0700

    ex01_is_done_Oct8_14-35

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 15:30:28 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/10/08 12:46:50 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/10/08 14:31:53 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,28 @@ void trySignForm(Bureaucrat bure, Form form)
 	}
 }
 
+void tryCreateSignFormOnHeap(Bureaucrat bure, int form_grade) {
+	try {
+		Form* form = new Form("WTForm", form_grade, 1);
+		std::cout << *form;
+		trySignForm(bure, *form);
+		delete form;
+	}
+	catch (std::exception& e) {
+		std::cerr << "Error: unable to create Form: " << e.what() << std::endl;
+	}
+}
+
 void onHeap(int bure_grade, int form_grade)
 {
-	if (form_grade > bure_grade) {
+	if (bure_grade < form_grade) {
+		std::cout << "====== ON HEAP ======" << std::endl;
 		for (int i = form_grade; bure_grade <= i;) {
 			std::cout << std::endl;
 			try {
 				Bureaucrat* bure = new Bureaucrat("Bureaucrat", bure_grade);
 				std::cout << "Created: " << *bure << std::endl;
-				try {
-					Form* form = new Form("WTForm", form_grade, 1);
-					std::cout << *form;
-					trySignForm(*bure, *form);
-					delete form;
-				}
-				catch (std::exception& e) {
-					std::cerr << "Error: unable to create Form: " << e.what() << std::endl;
-				}
+				tryCreateSignFormOnHeap(*bure, form_grade);
 				delete bure;
 			}
 			catch (std::exception& e) {
@@ -53,22 +58,29 @@ void onHeap(int bure_grade, int form_grade)
 		std::cerr << "Error: incorrect arguments" << std::endl;
 }
 
+void tryCreateSignFormOnStack(Bureaucrat bure, int form_grade)
+{
+	try {
+		Form form("WTForm", form_grade, 1);
+		std::cout << form;
+		trySignForm(bure, form);
+	}
+	catch (std::exception& e) {
+		std::cerr << "Error: unable to create Form: " << e.what() << std::endl;
+	}
+		
+}
+
 void onStack(int bure_grade, int form_grade)
 {
 	if (form_grade > bure_grade) {
+		std::cout << "====== ON STACK ======" << std::endl;
 		for (int i = form_grade; bure_grade <= i;) {
 			std::cout << std::endl;
 			try {
 				Bureaucrat bure("Bureaucrat", bure_grade);
 				std::cout << "Created: " << bure << std::endl;
-				try {
-					Form form("WTForm", form_grade, 1);
-					std::cout << form;
-					trySignForm(bure, form);
-				}
-				catch (std::exception& e) {
-					std::cerr << "Error: unable to create Form: " << e.what() << std::endl;
-				}
+				tryCreateSignFormOnStack(bure,form_grade);
 			}
 			catch (std::exception& e) {
 				std::cerr << "Error: Unable to create Bureaucrat: " << e.what() << std::endl;
